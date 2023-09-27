5b5d817d3a98a26ed4456ca6364304ba7d7b5e1d
wprice-icc <142839448+wprice-icc@users.noreply.github.com>
2023-09-27 06:44:37 -0500

    Create m6.py

---
+++ m6.py
@@ -0,0 +1,58 @@
+1. Read from a File
+
+Description: Read and print the contents of a file.
+Variables: filename, content
+
+
+
+2. Count Lines in a File
+
+Description: Count the number of lines in a file.
+Variables: filename, line_count
+
+
+
+3. Append to File
+
+Description: Append a given string to a file.
+Variables: filename, text
+
+
+
+4. Read N Lines
+
+Description: Read the first n lines from a file.
+Variables: filename, n, lines
+
+
+
+5. Delete a Line
+
+Description: Delete the nth line from a file.
+Variables: filename, n
+
+
+
+6. Sum Numbers
+
+Description: Assume a file contains a list of numbers (one per line). Write a function to sum these numbers.
+Variables: filename
+
+
+
+7. Find the Longest Word
+
+Description: Find and return the longest word from a file.
+Variables: filename
+
+
+
+8. Exception Handling
+
+Description: Handle the exception if a file doesn't exist.
+Variables: filename
+
+
+
+
+
